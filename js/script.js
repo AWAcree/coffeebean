9cefcf39fe63e3dfc23df4dadc586afd66a2f06d
AWAcree <mr.terrificaa@icloud.com>
2021-07-20 23:20:57 -0400

    form submit js

--- js/script.js
+++ js/script.js
@@ -14,5 +14,89 @@ function initMap(){
       position:{lat:38.240850, lng:-85.720530},
       map:map
   });
- 
-      }
+    var infoWindow = new google.maps.InfoWindow({
+        content: '<h1>The Coffee Bean</h1>'
+    });
+
+    marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+    });
+ }
+
+
+
+
+const isValidEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const isValidPhone = (phone) => {
+  const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(String(phone).toLowerCase());
+};
+
+const form = document.querySelector("form");
+const thankYou = document.querySelector(".thank-you");
+const nameInput = document.querySelector('input[name="firstname"]');
+const lastnameInput = document.querySelector('input[name="lastname"]');
+const emailInput = document.querySelector('input[name="email"]');
+
+const inputs = [
+    nameInput,
+    lastnameInput,
+    emailInput
+]
+let isFormValid = false;
+let isValidationOn = false;
+
+const resetElm = (elm) => {
+   elm.nameInput.classList.remove('invalid');
+   elm.nameInput.nextElementSibling.classList("hidden");
+};
+
+const invalidateElm = (elm) => {
+    elm.classList.add("invalid");
+    elm.nextElementSibling.classList.remove("hidden");
+};
+
+const validateInputs = () => {
+    if (!isValidationOn) return;
+    isFormValid = true;
+    resetElm(nameInput);
+    resetElm(lastnameInput);
+    resetElm(emailInput);
+
+    if (!nameInput.value) {
+         isFormValid = false;
+        invalidateElm(nameInput);
+    }
+    if (!lastnameInput.value) {
+        isFormValid = false;
+       invalidateElm(nameInput);
+   }
+   if (!isValidLastname(lastnameInput.value)) {
+    isFormValid = false;
+   invalidateElm(lastNameInput);
+}
+    if (!isValidEmail(emailInput.value)) {
+        isFormValid = false;
+       invalidateElm(emailInput);
+   }
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    isValidationOn = true;
+    validateInputs();
+    if (isFormValid) {
+        form.remove();
+        thankYou.classList.remove("hidden");
+    }
+});
+
+inputs.forEach((input) => 
+    input.addEventListener("input",
+        validateInputs));
+    
+
